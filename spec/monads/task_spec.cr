68bde01ba377142c1d438760cee86f0226659740
Alexandre Lairan <lairana@free.fr>
2019-05-13 18:55:55 +0200

    Implement task monad

---
+++ spec/monads/task_spec.cr
@@ -0,0 +1,40 @@
+require "../spec_helper"
+
+class Exception
+  def <=>(other)
+    0
+  end
+end
+
+describe Monads::Task do
+  describe "#bind" do
+    it "Task have correct element" do
+      monad = Monads::Task(Int32).new(-> { 5 })
+      monad.bind ->(number : Int32) { number.should eq(5) }
+    end
+  end
+
+  describe "#to_maybe" do
+    it "Monads::Task(Int32).new(-> { 5 }).to_maybe is Just" do
+      value = Monads::Task(Int32).new(-> { 5 }).to_maybe
+      value.should eq(Monads::Just.new(5))
+    end
+
+    it "Monads::Task(Int32).new(-> { 5 / 0 }).to_maybe is Nothing" do
+      value = Monads::Task(Int32).new(-> { 5 / 0 }).to_maybe
+      value.should eq(Monads::Nothing(Int32).new)
+    end
+  end
+
+  describe "#to_either" do
+    it "Monads::Task(Int32).new(-> { 5 }).to_either is Right" do
+      value = Monads::Task(Int32).new(-> { 5 }).to_either
+      value.should eq(Monads::Right.new(5))
+    end
+
+    it "Monads::Task(Int32).new(-> { 5 / 0 }).to_either is Left" do
+      value = Monads::Task(Int32).new(-> { 5 / 0 }).to_either
+      value.should eq(Monads::Left(Exception).new(DivisionByZeroError.new))
+    end
+  end
+end
---
+++ src/monads/task.cr
@@ -0,0 +1,71 @@
+module Monads
+  struct Task(T)
+    enum Message
+      None,
+      Value,
+      Error
+    end
+
+    class InvalidMessage < Exception
+      def initialize(msg = "Message is invalid")
+        super(msg)
+      end
+    end
+
+    @final_value : T?
+    @final_error : Exception?
+
+    @kind = Message::None
+
+    def initialize(proc)
+      @channel_message = channel_message = Channel::Buffered(Message).new
+      @channel_value = channel_value = Channel::Buffered(T).new
+      @channel_error = channel_error = Channel::Buffered(Exception).new
+      @final_value = nil
+      @final_error = nil
+
+      spawn do
+        begin
+          value = proc.call
+          channel_message.send Message::Value
+          channel_value.send value
+        rescue exception
+          channel_message.send Message::Error
+          channel_error.send exception
+        end
+      end
+    end
+
+    def bind(lambda : T -> U) : U forall U
+      lambda.call(receive)
+    end
+
+    def receive : T
+      case @kind
+      when Message::None
+        @kind = @channel_message.receive
+        receive
+      when Message::Value
+        @final_value = @channel_value.receive unless @final_value
+        @final_value.not_nil!
+      when Message::Error
+        @final_error = @channel_error.receive unless @final_error
+        raise @final_error.not_nil!
+      else
+        raise InvalidMessage.new
+      end
+    end
+
+    def to_maybe
+      Just.new receive
+    rescue
+      Nothing(T).new
+    end
+
+    def to_either
+      Right.new receive
+    rescue exception
+      Left.new exception
+    end
+  end
+end
