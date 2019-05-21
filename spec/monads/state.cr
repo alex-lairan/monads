ba70435aa174811ea92dc298150754d6ed784cb4
moba1 <idea.care.origin@gmail.com>
2019-05-21 11:44:08 +0900

    fix test to type explicitly

--- spec/monads/state.cr
+++ spec/monads/state.cr
@@ -3,7 +3,7 @@ require "../../spec_helper"
 describe Monads::State do
   describe "#run" do
     it "State.return(3).run(\"state\") == {3, \"state\"}" do
-      state = Monads::State.return(3).run("state")
+      state = Monads::State(String, Int32).return(3).run("state")
       state.should eq({3, "state"})
     end
   end
