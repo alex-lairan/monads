3ee228ba9e061ddd7bc7a9e5c48712deeece78cc
moba1 <idea.care.origin@gmail.com>
2019-05-21 11:32:51 +0900

    add State monad test

---
+++ spec/monads/state.cr
@@ -0,0 +1,10 @@
+require "../../spec_helper"
+
+describe Monads::State do
+  describe "#run" do
+    it "State.return(3).run(\"state\") == {3, \"state\"}" do
+      state = Monads::State.return(3).run("state")
+      state.should eq({3, "state"})
+    end
+  end
+end
