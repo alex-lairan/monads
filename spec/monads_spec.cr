cd616b9159e21a9ee06c370de93ffcd080c77cc6
Alexandre Lairan <lairana@free.fr>
2019-08-03 09:24:24 +0200

    Bump version to 0.5.1

--- spec/monads_spec.cr
+++ spec/monads_spec.cr
@@ -3,6 +3,6 @@ require "./monads/**"
 
 describe Monads do
   it "should have version" do
-    Monads::VERSION.should eq("0.5.0")
+    Monads::VERSION.should eq("0.5.1")
   end
 end
--- src/monads.cr
+++ src/monads.cr
@@ -3,5 +3,5 @@ require "./monads/*"
 module Monads
   # Requirements for 1.0.0
   # - Documentation
-  VERSION = "0.5.0"
+  VERSION = "0.5.1"
 end
